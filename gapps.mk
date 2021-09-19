9b703c77ab9bed0f3d4fb30a14647c538cacc4bb
rahonen <riku.aj.ahonen@outlook.com>
2021-09-19 20:10:38 +0300

    testing 2

--- gapps.mk
+++ gapps.mk
@@ -33,11 +33,6 @@ GAPPS_FORCE_PACKAGE_OVERRIDES := true
 GAPPS_FORCE_BROWSER_OVERRIDES := true
 GAPPS_FORCE_WEBVIEW_OVERRIDES := true
 PRODUCT_PACKAGES += \
-       Chrome \
-       CalculatorGoogle \
-       PrebuiltDeskClockGoogle \
-       CalendarGooglePrebuilt \
-       LatinImeGoogle \
        phh-overrides
 
 $(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)

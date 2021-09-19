5a93d47ba09da2cf2086f313de67cae8748a382d
rahonen <riku.aj.ahonen@outlook.com>
2021-09-19 23:43:01 +0300

    test

--- gapps.mk
+++ gapps.mk
@@ -29,8 +29,6 @@ PRODUCT_PACKAGE_OVERLAYS += device/phh/treble/overlay-gapps
 GAPPS_VARIANT := pico
 DONT_DEXPREOPT_PREBUILTS := true
 WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-GAPPS_FORCE_PACKAGE_OVERRIDES := true
-GAPPS_FORCE_WEBVIEW_OVERRIDES := true
 
 $(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
 endif

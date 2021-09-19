1375dc510d8a146d99fbecdf9a19b3a100c92f3c
rahonen <riku.aj.ahonen@outlook.com>
2021-09-19 20:15:19 +0300

    testing 3

--- gapps.mk
+++ gapps.mk
@@ -30,10 +30,7 @@ GAPPS_VARIANT := pico
 DONT_DEXPREOPT_PREBUILTS := true
 WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
 GAPPS_FORCE_PACKAGE_OVERRIDES := true
-GAPPS_FORCE_BROWSER_OVERRIDES := true
 GAPPS_FORCE_WEBVIEW_OVERRIDES := true
-PRODUCT_PACKAGES += \
-       phh-overrides
 
 $(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
 endif
